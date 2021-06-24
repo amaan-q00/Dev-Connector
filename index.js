0fe2ecb5c11808cc8804a0e27a949554b3cf2aee
Amaanx2056x <weirdghost123@gmail.com>
2021-06-24 18:55:08 +0530

    added postbuild script to prepare for deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 require('./config/db-config')
+const path=require('path')
 const port = process.env.PORT || 2000
 
 //middlewares
@@ -14,7 +15,12 @@ app.use('/api/posts', require("./routes/api/post"))
 app.use('/api/profile', require("./routes/api/profile"))
 app.use('/api/auth', require("./routes/api/auth"))
 
-
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static('client/build'))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+  })
+}
 
 app.listen(port, ()=> {
   console.log(`Server is running on port ${port}`)
